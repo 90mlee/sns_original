43f1a1baf9f115fcc57ae0443426e29d3d851643
SKCC22N00554\Administrator <90mlee@gmail.com>
2023-02-01 00:37:51 +0900

    1st commit/push. ch06 done

---
+++ src/main/java/com/fastcampus/snsproject/util/JwtTokenUtils.java
@@ -0,0 +1,36 @@
+package com.fastcampus.snsproject.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+
+public class JwtTokenUtils {
+
+    public static String generateToken(String userName, String key, long expiredTimeMs) {
+        Claims claims = Jwts.claims();
+        claims.put("userName", userName);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiredTimeMs))
+                .signWith(getSigningKey(key), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    //private static String doGenerateToken(String username, long expireTime, String key) {
+   //
+   // }
+
+    private static Key getSigningKey(String secretKey) {
+        byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+}

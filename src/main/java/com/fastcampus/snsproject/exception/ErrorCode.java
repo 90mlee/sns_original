5295eababad35e5209a64e2f5f73902ce0c800aa
SKCC22N00554\Administrator <90mlee@gmail.com>
2023-02-08 16:59:41 +0900

    kafka connected but alarm does not work. assignment done.

--- src/main/java/com/fastcampus/snsproject/exception/ErrorCode.java
+++ src/main/java/com/fastcampus/snsproject/exception/ErrorCode.java
@@ -15,7 +15,7 @@ public enum ErrorCode {
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "Post not founded"),
     INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "Permission is invalid"),
     ALREADY_LIKED(HttpStatus.CONFLICT, "User already like the post"),
-
+    ALARM_CONNECT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Alarm connection error occurs"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
     ;
 
